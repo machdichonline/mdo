b4b7b92b3f160e2aa64c1aef169cc381194466a5
machdichonline <kontakt@mach-dich-online.de>
2026-04-19 14:22:48 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -158,7 +158,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const headerLogo = document.querySelector('.header-logo');
         const headerLogoImg = headerLogo?.querySelector('img');
 
-        if (!isHomePage || !headerLogo || !headerLogoImg) {
+        if (!headerLogo || !headerLogoImg) {
             document.body.classList.remove('loading');
             if (headerLogo) headerLogo.style.opacity = '1';
             return;
